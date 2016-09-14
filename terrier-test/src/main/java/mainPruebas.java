4336888c6b0b8a8cda973c12081b9efdf7badb48
jetchart <juanmetchart@gmail.com>
2016-09-14 16:33:56 -0300

    Cambios: 	- Se agrega funcionalidad en "mainPruebas.java" para crear un corpus donde los documentos aparecen varias veces en el mismo archivo (hace tipo un merge).

--- terrier-test/src/main/java/mainPruebas.java
+++ terrier-test/src/main/java/mainPruebas.java
@@ -1,71 +1,92 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-import org.terrier.realtime.memory.MemoryDocumentIndexMap;
-import org.terrier.structures.Index;
-import org.terrier.structures.Lexicon;
-import org.terrier.structures.LexiconEntry;
-import org.terrier.structures.PostingIndex;
-import org.terrier.structures.postings.IterablePosting;
+import org.apache.log4j.Logger;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 
 public class mainPruebas {
 
+	static final Logger logger = Logger.getLogger(mainPruebas.class);
+	
 	public static void main(String[] args) throws IOException, ParserConfigurationException, SAXException {
-		System.out.println("Levantando índice..");
-		Index index = Index.createIndex("/storage/juan/terrier-4.0/var/index/previousIndex/", "noProcess_partitioning.CRoundRobinByDocuments");
-		System.out.println("Índice levantado");
-		Integer cantidadDocumentos = index.getCollectionStatistics().getNumberOfDocuments();
-		String fullPath = "/storage/juan/TERRIER_JAVA/master/corpus.trec";
-	    PostingIndex<?> postingIndex = index.getInvertedIndex();
-		Lexicon<String> mapLexicon = index.getLexicon();
-		StringBuffer contenido = new StringBuffer();
-		System.out.println("Total de documentos: " + cantidadDocumentos);
-		for (Integer documentId=0;documentId<cantidadDocumentos;documentId++){
-			System.out.println("Procesando documento: " + documentId);
-			Map<String, Integer> map = new HashMap<String, Integer>();
-			for (Entry<String, LexiconEntry> lexicon : mapLexicon){
-				IterablePosting iterablePosting = postingIndex.getPostings(lexicon.getValue());
-				while (!iterablePosting.endOfPostings()){
-					iterablePosting.next();
-					if (iterablePosting.getId() == documentId ){
-						map.put(lexicon.getKey(), iterablePosting.getFrequency());
-						break;
+		
+		/* ACLARACION */
+		/* Si al corpus source le faltan las etiquetas de <ROOT></ROOT> utilizar el siguiente script:
+		  		sed -i '1i <ROOT>' /home/javier/TERRIER_JAVA/master/corpus/corpusSource.trec;
+				sed -i '$a </ROOT>' /home/javier/TERRIER_JAVA/master/corpus/corpusSource.trec;
+		*/
+		String sourcePath = args[0]; /* --> Desde qué archivo se extraerán los datos */
+		String targetPath = args[1]; /* --> Hacia qué archivo se guardarán los datos */
+		Long cantidadMaximaDocumentosProcesados = Long.valueOf(args[2]); /* --> Indica la cantidad máxima de documentos procesados antes de impacar en archivo físico */
+//		String sourcePath = "/home/javier/TERRIER_JAVA/master/corpus/corpus0_run10_partitioning.CRoundRobinByTerms_1_false_PATH_true_2016-09-14_09:58:50.936.trec";
+//		String targetPath = "/home/javier/TERRIER_JAVA/master/corpus/resultado.trec";
+		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance(); 
+		DocumentBuilder docBuilder = docBuilderFactory.newDocumentBuilder(); 
+		Document documento = (Document) docBuilder.parse (new File(sourcePath));
+		documento.getDocumentElement().normalize ();
+		logger.info("El elemento raíz es " + documento.getDocumentElement().getNodeName());
+		NodeList listaDOC = documento.getElementsByTagName("DOC");
+		logger.info("Cantidad de documentos a procesar: " + listaDOC.getLength());
+		Collection<String> docNoProcesados = new ArrayList<String>();
+		Map<String,StringBuffer> docContenidos = new HashMap<String,StringBuffer>();
+		Long cantidadDocumentosProcesados = 0L;
+		for (Integer i=0;i<listaDOC.getLength();i++){
+			Node docPrincipal = listaDOC.item(i);
+			Element elementoPrincipal = (Element) docPrincipal;
+			String docNoPrincipal = elementoPrincipal.getElementsByTagName("DOCNO").item(0).getTextContent();
+			if (!docNoProcesados.contains(docNoPrincipal)){
+				cantidadDocumentosProcesados++;
+				StringBuffer contenido = new StringBuffer();
+				for (Integer e=0;e<listaDOC.getLength();e++){
+					Node doc = listaDOC.item(e);
+					Element elemento = (Element) doc;
+					String docNo = elemento.getElementsByTagName("DOCNO").item(0).getTextContent();
+					String texto = doc.getTextContent().replace("DOCNO:", "").replace(docNo, "").replace("\n", "");
+					if (docNo.equals(docNoPrincipal)){
+						contenido.append(" ").append(texto);
 					}
 				}
+				logger.info("Documento procesado: " + docNoPrincipal);
+				logger.info("Cantidad documentos procesados: " + docNoProcesados.size());
+//				System.out.println("contenido: " + contenido);
+				docNoProcesados.add(docNoPrincipal);
+				docContenidos.put(docNoPrincipal,contenido);
+				if (cantidadDocumentosProcesados > cantidadMaximaDocumentosProcesados){
+					write(targetPath,docContenidos);
+					docContenidos = new HashMap<String,StringBuffer>();
+					cantidadDocumentosProcesados = 0L;
+				}
 			}
-	        contenido.append("<DOC>\n");
-	        contenido.append("<DOCNO>"+ documentId +"</DOCNO>\n");
-	        /* Terminos */
-	        for (String term : map.keySet()){
-	        	Integer freq =  map.get(term);
-	        	for (Integer i=0;i<freq;i++){
-	        		contenido.append(term).append(" ");
-	        	}
-	        }
-	        contenido.append("\n</DOC>\n");
-	        contenido.append("\n");
-	        if (contenido.length() > 1073741824L){
-	        	write(fullPath, contenido);
-	        	contenido = new StringBuffer();
-	        }
 		}
-        if (contenido.length() > 0){
-        	write(fullPath, contenido);
-        }
+		if (cantidadDocumentosProcesados > 0){
+			write(targetPath,docContenidos);
+		}
 	}
 
-	private static void write(String fullPath, StringBuffer contenido) throws IOException {
+	private static void write(String fullPath, Map<String,StringBuffer> docContenidos) throws IOException {
 		FileWriter fichero = new FileWriter(fullPath,Boolean.TRUE);
 		PrintWriter pw = new PrintWriter(fichero);
-		pw.print(contenido);
+		for (String docNo : docContenidos.keySet()){
+			pw.print("<DOC>\n");
+			pw.print("<DOCNO>"+docNo+"</DOCNO>\n");
+			pw.print(docContenidos.get(docNo)+"\n");
+			pw.print("</DOC>\n");
+		}
 		pw.close();
 		fichero.close();
 	}
--- terrier-test/src/main/java/partitioning/IPartitionByTerms.java
+++ terrier-test/src/main/java/partitioning/IPartitionByTerms.java
@@ -5,7 +5,7 @@ import java.util.Map;
 
 public interface IPartitionByTerms extends IPartitionMethod {
 	
-	public static final Long cantidadMaximaTokensAntesCierre = 50000000L; 
+	public static final Long cantidadMaximaTokensAntesCierre = 100000000L; 
 	
 	public void writeDoc(Map<Long, Map<Long, Map<String, Long>>> mapNodeDocTerm, Integer cantidadCorpus, String destinationFolderPath, List<String> colCorpusTotal);
 	
